7935b4ee7bc4a915d36542adbd6e8a1c6f83960f
olinstru <o.linstrumelle@betclicgroup.com>
2023-08-23 13:59:01 +0200

    Premier commit du projet

---
+++ script.js
@@ -0,0 +1,53 @@
+function createObjectFromUserInput(userTitle, userDescription, userDeadline, userFile) {
+    return {
+        title: userTitle,
+        description: userDescription,
+        deadline: userDeadline,
+        file: userFile,
+    };
+}
+
+let tasks = new Array();
+
+function addTask() {
+    let task = createObjectFromUserInput(
+        document.getElementById("taskTitle").value,
+        document.getElementById("taskDescription").value,
+        document.getElementById("taskDeadline").value,
+        document.getElementById("taskFile").value
+    );
+
+    if (!task) {
+        alert("Please add a task");
+    }
+
+    tasks.push(task);
+    console.log(tasks)
+
+    document.getElementById("tasks").innerHTML = ""
+
+    insertTask(tasks)
+}
+
+function deleteTask(taskToDelete) {
+    tasks.splice(tasks.indexOf(taskToDelete), 1)
+
+    console.log(tasks)
+}
+
+function insertTask(tasks) {
+    tasks.forEach((element, index) => {
+        document.getElementById("tasks").innerHTML += `
+            <li id="${element.title}-${index}"> 
+            ${element.title}<br>
+            ${element.description}<br>
+            Deadline: ${element.deadline}<br>
+            Image: ${element.file}<br>
+            <button id="deleteButton" onclick="deleteTask()">Delete</button>
+            </li>
+        `
+    });
+}
+
+// Click on a delete button to hide the current list item
+
