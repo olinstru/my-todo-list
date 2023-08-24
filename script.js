e6ca3ee3fe6cc8d765e05547cfe5843d29164a91
olinstru <o.linstrumelle@betclicgroup.com>
2023-08-24 13:27:42 +0200

    corrected showList() function

--- script.js
+++ script.js
@@ -28,29 +28,30 @@ function addTask() {
 }
 
 function showList(tasks) {
-    var listItem = tasks.forEach((element, index) => {
-        document.getElementById("tasks").innerHTML += `
-            <li id="${element.title}-${index}"> 
-            ${element.title}<br>
-            ${element.description}<br>
-            Deadline: ${element.deadline}<br>
-            Image: ${element.file}<br>
-            <button id="deleteButton" onclick="deleteTask()">Delete</button>
-            </li>
-        `
-    });
+    var listItems = tasks.map((element, index) => `
+        <li id="${element.title}-${index}"> 
+        ${element.title}<br>
+        ${element.description}<br>
+        Deadline: ${element.deadline}<br>
+        Image: ${element.file}<br>
+        <button id="deleteButton" onclick="deleteTask(${index})">Delete</button>
+        </li>
+    `);
+
+    document.getElementById("tasks").innerHTML = listItems.join("");
 }
+
 
 function deleteTask() {
     taskToDelete = task.indexOf()
     console.log(taskToDelete)
+}
 
-    // let taskId = document.getElementById(`"${element.title}-${index}"`);
+// let taskId = document.getElementById(`"${element.title}-${index}"`);
    // console.log(tasks.indexOf(task))
     /*   let taskToDelete = taskId;
       tasks.splice(taskToDelete, 1)
           console.log(tasks) */
-}
 
 
 /* function deleteTask(taskToDelete) {
