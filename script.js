165d52ec5ec59ffb48047eb821fd85a8dd14cc83
olinstru <o.linstrumelle@betclicgroup.com>
2023-08-25 12:46:40 +0200

    added a new function currentBase64 to store image (now works correctly but all photos are replaced on new upload- needs fixing)

--- script.js
+++ script.js
@@ -33,6 +33,8 @@ function clearInputFields() {
             reader.readAsDataURL(file);
 } */
 
+let currentBase64 = ""; // Declare a global variable
+
 // Function to convert image to Base64 string
 function onChange() {
     let userImg = document.getElementById("taskFile");
@@ -41,11 +43,8 @@ function onChange() {
         const reader = new FileReader();
 
         reader.onloadend = function() {
-            const base64 = reader.result;
-            document.getElementById('outputBase64').value = base64;
-
-            // Call showList after loading the Base64
-            // showList(tasks, base64);
+            currentBase64 = reader.result; // Store the base64 value
+            showList(tasks, currentBase64); // Call showList after loading the Base64
         };
 
         reader.readAsDataURL(file);
@@ -58,7 +57,7 @@ function addTask() {
         document.getElementById("taskTitle").value,
         document.getElementById("taskDescription").value,
         document.getElementById("taskDeadline").value,
-        document.getElementById("taskFile").value
+        currentBase64 // Use the stored base64 value here
     );
 
     if (!task) {
@@ -68,8 +67,8 @@ function addTask() {
     tasks.push(task);
     console.log(tasks);
 
-    document.getElementById("tasks").innerHTML = ""
-    showList(tasks)
+    document.getElementById("tasks").innerHTML = "";
+    showList(tasks, currentBase64); // Pass the base64 value to showList
 
     clearInputFields(); // Call the function to clear inputs
 }
@@ -78,11 +77,13 @@ function addTask() {
 function showList(tasks, base64) {
     var listItems = tasks.map((element, index) => `
         <li id="${element.title}-${index}"> 
-        ${element.title}<br>
+        <h2>${element.title}</h1><br>
         ${element.description}<br>
+        <br>
         Deadline: ${element.deadline}<br>
-        Image:<br> ${element.file ? `<img id="userImg" src="${base64}">
+        <br> ${element.file ? `<img id="userImg" src="${base64}">
         <br>` : 'No image available<br>'}
+        <br>
         <button id="deleteButton" onclick="deleteTask(${index})">Delete</button>
         </li>
     `);
